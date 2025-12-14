189cbc44966141fce2880e50419c8c408771fec9
beaNoBeebea <abirfakhreddine@gmail.com>
2025-12-14 20:33:22 +0100

    idk

--- deliverable_5/sql/triggers/prevent_bad_stock.sql
+++ deliverable_5/sql/triggers/prevent_bad_stock.sql
@@ -2,10 +2,16 @@ DROP TRIGGER IF EXISTS check_stock_ins;
 DROP TRIGGER IF EXISTS check_stock_upd;
 
 
+-- three business rules:
+-- Quantity cannot be negative
+-- Unit price must be strictly positive
+-- Reorder level cannot be negative
+
 DELIMITER $$
 
+-- a trigger before insert
 CREATE TRIGGER check_stock_ins
-BEFORE INSERT ON Stock
+BEFORE INSERT ON Stock  -- it runs before a new row is inserted in the table, so it can block the insert if something goes wrong
 FOR EACH ROW
 BEGIN 
     IF NEW.Qty < 0 THEN
@@ -24,6 +30,7 @@ DELIMITER ;
 
     
 DELIMITER $$
+-- a trigger before update
         CREATE TRIGGER check_stock_upd BEFORE UPDATE ON Stock
         For each row
         BEGIN 
